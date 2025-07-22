3507272a95a74c2b70c0cec847d66d59f92f7c8c
akahard13 <harnaldosanchez49@gmail.com>
2025-07-22 11:12:31 -0600

    prueba

--- resources/js/Pages/Planes.jsx
+++ resources/js/Pages/Planes.jsx
@@ -28,8 +28,6 @@ const Planes = ({ clientes, auth }) => {
         cliente && cliente.codigo && cliente.codigo.toLowerCase().includes(searchTerm.toLowerCase()) ||
         cliente && cliente.id && cliente.id.toString().includes(searchTerm)
     );
-
-    const { flash } = usePage().props;
     const rol = auth.user.rol;
     return (
         <AuthenticatedLayout
@@ -48,8 +46,6 @@ const Planes = ({ clientes, auth }) => {
                     Nuevo Cliente
                 </Link>
             )}
-            {flash.success && <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4 mt-4">{flash.success}</div>}
-            {flash.permission && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4 mt-4">{flash.permission}</div>}
             {/* Campo de búsqueda */}
             <div className="my-4">
                 <input
@@ -84,15 +80,18 @@ const Planes = ({ clientes, auth }) => {
                                     <td className="text-left px-4 py-2">{cliente.codigo}</td>
                                     <td className="text-left px-4 py-2">{cliente.servicio}</td>
                                     <td className="text-left px-4 py-2">
-                                        {new Intl.DateTimeFormat('es-ES', {
-                                            day: '2-digit',
-                                            month: '2-digit',
-                                            year: 'numeric',
-                                            timeZone: 'UTC'  // 👈 Fuerza el formato UTC
-                                        }).format(new Date(cliente.fecha_vencimiento + 'Z'))}
-
-
+                                        {cliente.fecha_vencimiento && !isNaN(new Date(cliente.fecha_vencimiento)) ? (
+                                            new Intl.DateTimeFormat('es-ES', {
+                                                day: '2-digit',
+                                                month: '2-digit',
+                                                year: 'numeric',
+                                                timeZone: 'UTC',
+                                            }).format(new Date(cliente.fecha_vencimiento + 'Z'))
+                                        ) : (
+                                            'Sin fecha'
+                                        )}
                                     </td>
+
                                     <td className="text-left px-4 py-2">
                                         <span
                                             className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-md ${cliente.estado ? "bg-orange-100 text-orange-800" : "bg-red-100 text-red-800"}`}
